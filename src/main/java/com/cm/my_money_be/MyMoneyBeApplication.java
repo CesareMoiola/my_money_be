469d17c9558c8e41aeeac1a47be91138ad2c21b9
Cesare Moiola <cesaremoiola@gmail.com>
2022-05-03 12:31:19 +0200

    initial commit

---
+++ src/main/java/com/cm/my_money_be/MyMoneyBeApplication.java
@@ -0,0 +1,13 @@
+package com.cm.my_money_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoneyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoneyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cm/my_money_be/MyMoneyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.my_money_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoneyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
